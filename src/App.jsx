e84676a67880ee9466b92e326ef177b3fa001048
farhancodes <pmdfarhan@gmail.com>
2025-02-09 21:00:23 +0530

    Fixed height issue

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import DevList from "./components/DevList";
 
 function App() {
   return (
-    <div className="min-h-svh flex flex-col bg-white">
+    <div className="flex flex-col bg-white">
       <h1 className="text-black text-3xl font-bold">
         <b className="text-green-600">Dev</b> list
       </h1>
       <DevList />
-      <footer className="text-black mt-6">
+      <footer className="text-black text-center mt-6">
         Made with <span className="text-green-600">&hearts;</span> by{" "}
-        <b className="font-bold">Farhan</b>
+        <b className="text-bold">Farhan</b>
       </footer>
     </div>
   );
