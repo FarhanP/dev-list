9864295d0dcd282aafce2d0ee746ee9e00e75875
farhancodes <pmdfarhan@gmail.com>
2025-02-09 21:04:50 +0530

    Footer changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <DevList />
       <footer className="text-black text-center mt-6">
         Made with <span className="text-green-600">&hearts;</span> by{" "}
-        <b className="font-bold">Farhan</b>
+        <span className="font-bold">Farhan</span>
       </footer>
     </div>
   );
